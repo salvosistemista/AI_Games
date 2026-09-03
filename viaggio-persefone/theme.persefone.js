ae9e6a7fe176fa55c969516a2a9f7ccaeedf1f4c
salvosistemista <105580772+salvosistemista@users.noreply.github.com>
2026-09-03 18:51:38 +0200

    Add files via upload

--- viaggio-persefone/theme.persefone.js
+++ viaggio-persefone/theme.persefone.js
@@ -3,8 +3,8 @@
    ============================================================ */
 
 const THEME = {
-    title: "Nebbie su Blackthorn Hall",
-    titleHtml: "NEBBIE SU<br>BLACKTHORN HALL<br><span style='font-size:12px;color:#7a5c2e;'>Un'avventura gotica in cinque atti</span>",
+    title: "L'Ultimo Viaggio del Persefone",
+    titleHtml: "L'Ultimo Viaggio<br>del Persefone<br><span style='font-size:12px;color:#7a5c2e;'>Un'avventura gotica in cinque atti</span>",
     versionTag: "Alderbrook, 1887",
     colorMain: "#c9a227",
     colorDim: "#7a5c2e",
